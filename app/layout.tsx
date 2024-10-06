527c0bb0ac722d96908a46d702486ef7825f1f57
mhasnain23 <tryfrelancer23@gmail.com>
2024-10-06 23:26:50 +0500

    nit

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import "./globals.css";
 import Header from "@/components/header";
 import PageTransition from "@/components/PageTransition";
 import StairTransition from "@/components/StairTransition";
+import { Analytics } from "@vercel/analytics/react"
 
 const jetbrainsMono = JetBrains_Mono({
   subsets: ["latin"],
@@ -26,7 +27,10 @@ export default function RootLayout({
       <body className={`${jetbrainsMono.variable} antialiased`}>
         <Header />
         <StairTransition/>
-        <PageTransition>{children}</PageTransition>
+        <PageTransition>
+          <Analytics framework="next.js"/>
+          {children}
+          </PageTransition>
       </body>
     </html>
   );
