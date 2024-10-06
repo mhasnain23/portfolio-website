865d3ea9bf241098f03654221db2abd3e66fc347
mhasnain23 <tryfrelancer23@gmail.com>
2024-10-06 22:20:36 +0500

    nut

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -195,12 +195,18 @@ const Contact = () => {
               {info.map((item, index) => {
                 return (
                   <li key={index} className="flex items-center gap-6">
-                    <div className="w-[52px] h-[52px] xl:w-[72px] xl:h-[72px] bg-[#27272c] text-[#00ff99] rounded-[6px] flex items-center justify-center mt-4">
-                      <div className="text-[28px]">{item.icon}</div>
+                    <div className="w-[42px] h-[42px] xl:w-[72px] xl:h-[72px] bg-[#27272c] text-[#00ff99] rounded-[6px] flex items-center justify-center mt-4">
+                      <div className="xl:text-[28px] text-[15px]">
+                        {item.icon}
+                      </div>
                     </div>
                     <div className="flex-1">
-                      <p className="text-white/60">{item.title}</p>
-                      <h3 className="text-xl">{item.description}</h3>
+                      <p className="text-white/60 xl:text-[20px] text-[10px]">
+                        {item.title}
+                      </p>
+                      <h3 className="text-[12px] xl:text-[22px]">
+                        {item.description}
+                      </h3>
                     </div>
                   </li>
                 );
